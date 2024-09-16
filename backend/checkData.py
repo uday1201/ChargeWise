a3c85c63c37c1572dec287a847a984ec231e4fe7
Berke Nuri <bnuri@teslaforecast.com>
2024-09-16 17:35:15 -0400

    added tw3 forecasts

--- backend/checkData.py
+++ backend/checkData.py
@@ -1,16 +1,21 @@
 import sqlite3
 
+def check_data(cursor, table):
+    # Query the energy_data table
+    cursor.execute(f"SELECT * FROM {table}")
+    rows = cursor.fetchall()
+
+    # Print the retrieved data
+    for row in rows:
+        print(row)
+
 # Connect to SQLite database
-conn = sqlite3.connect('energy_data.db')
+conn = sqlite3.connect('backend/energy_data.db')
 cursor = conn.cursor()
 
-# Query the energy_data table
-cursor.execute("SELECT * FROM energy_data")
-rows = cursor.fetchall()
-
-# Print the retrieved data
-for row in rows:
-    print(row)
+check_data(cursor, 'energy_data')
+check_data(cursor, 'load_forecast')
+check_data(cursor, 'wx_forecast')
 
 # Close the connection
 cursor.close()
